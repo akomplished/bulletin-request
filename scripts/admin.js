254a5a7fe5d6ded1711ce263f84f434c0a32a373
akomplished <akomplished@gmail.com>
2015-10-08 13:51:19 -0700

    Commiting Site

---
+++ scripts/admin.js
@@ -0,0 +1,203 @@
+﻿(function () {
+    window.onLoadAuthJS = function () {
+        google.load("visualization", "1", { "callback": onVisualLoad });
+    };
+
+    onVisualLoad = function () {
+        var querystring = encodeURIComponent('select A, B, C, D, E, F, G, H, I, J, K, L where L = "Pending"');
+        var request = new google.visualization.Query('https://docs.google.com/a/mrpkedu.org/spreadsheets/d/1Df0VVPUabFqqMHJ96NUvEv0AI6mFtCzgeSb2VAiIFHM/gviz/tq?sheet=NewRequests&tq=' + querystring);
+        console.log(querystring);
+        request.send(handleRequest);
+    };
+
+    var handleRequest = function (response) {
+        var objs = [];
+        var result = $.parseJSON(response.getDataTable().toJSON());
+
+        for (var i = 0; i < result.rows.length; i++) {
+            var obj = {};
+            for (var o in result.cols) {
+                if (result.cols[o].type == 'datetime')
+                    obj[result.cols[o].label] = result.rows[i].c[o].f;
+                else if (result.cols[o].type == 'date')
+                    obj[result.cols[o].label] = result.rows[i].c[o].f;
+                else
+                    obj[result.cols[o].label] = result.rows[i].c[o].v;
+            }
+            objs.push(obj);
+        }
+
+        var groups = {};
+        for (var item in objs) {
+            var group = objs[item].Category;
+            if (!(group in groups)) {
+                var counter = 1;
+                groups[group] = {
+                    event: [objs[item]]
+                };
+            } else {
+                groups[group].event.push(objs[item]);
+            }
+        }
+        fillTable(groups, loadHtml);
+    };
+
+    function fillTable(groups, callback) {
+        var html = "";
+        for (var group in groups) {
+            var table = "<h5 class='ui-widget-header' style='margin: 0.2em 0 0 0; padding: 0.2em 0 0.2em 0.5em'>" + group + "</h5>";
+            table += "<table class=\"tblgroup\" cellspacing=\"0\" style=\"border-spacing: 0;\"><thead><tr>";
+            table += "<th><input type='checkbox' name='selectall' id='input-selectall' onchange='checkall(this)' /></th>"
+            table += "<th>TimeStamp</th>";
+            table += "<th>Submitted By</th>";
+            table += "<th>Organization</th>";
+            table += "<th>Run Dates</th>";
+            table += "<th>Message</th>";
+            table += "</tr></thead><tbody>";
+            for (var i = 0; i < groups[group].event.length; i++) {
+                table += "<tr class='row-display' data-row='" + groups[group].event[i].Row + "'>";
+                table += "<td><input type='checkbox' name='selected' id='input-selected' value='" + groups[group].event[i].Row + "' /></td>"
+                table += "<td>" + convertDate(groups[group].event[i].TimeStamp) + "</td>";
+                table += "<td>" + groups[group].event[i].SubmittedBy + "</td>";
+                table += "<td>" + groups[group].event[i].Organization + "</td>";
+                table += "<td id='rundates'>" + groups[group].event[i].RunDates + "</td>";
+                table += "<td>" + groups[group].event[i].Message + "</td></tr>";
+                table += "<tr class='row-details collapsed'><td colspan='6' style='background-color: rgba(155, 186, 156, 0.2); border: 1px solid #D1D1D1; padding: 10px 0 10px 0; '>";
+                table += "<div class='grid-parent'>"
+                table += "<form id='form_" + groups[group].event[i].Row + "' data-row='" + groups[group].event[i].Row + "'>";
+                table += "<div class='grid-25'>";
+                table += "<label>Event Name: </label><input type='text' name='EventName' id='input-eventname' value='" + groups[group].event[i].EventName + "' style='width: 100%'/>";
+                table += "<label>Event Location: </label><input type='text' name='EventLocation' id='input-eventlocation' value='" + groups[group].event[i].EventLocation + "' style='width: 100%'/>";
+                table += "<label>Event Date: </label><input type='text' name='EventDate' id='input-eventdate_" + groups[group].event[i].Row + "' class='eventdates' value='" + groups[group].event[i].EventDate + "' style='width: 60%'/>";
+                table += "</div>";
+                table += "<div class='grid-25'>";
+                table += "<label>Organization: </label><input type='text' name='Organization' id='input-organization' value='" + groups[group].event[i].Organization + "' style='width: 99%' readonly/>";
+                table += "<label>Bulletin Category: </label><select form='form_" + groups[group].event[i].Row + "' id='input-category' name='Category' style='width: 100%'><option value='CollegeCareer'>College and Career</option><option value='Counseling'>Counseling</option><option value='Club'>Club</option><option value='General'>General</option><option value='ClassOf'>Class Of</option><option value='SchoolEvent'>SchoolEvent</option><option value='Athletics'>Athletics</option></select>";
+                table += "</div>";
+                table += "<div class='grid-50'>";
+                table += "<label>Message: </label><textarea form='form_" + groups[group].event[i].Row + "' name='Message' id='input-message' rows='6' style='width: 100%; height: 79px; display: block;'>" + groups[group].event[i].Message + "</textarea>";
+                table += "</div>";
+                table += "<div class='grid-75 date-container' onclick=\"$('#input-rundates_" + groups[group].event[i].Row + "').focus();\">";
+                table += "<label>Run Dates: </label><input type='text' form='form_" + groups[group].event[i].Row + "' name='RunDates' id='input-rundates_" + groups[group].event[i].Row + "' class='rundates' value='" + groups[group].event[i].RunDates + "' style='width: 100%;'/>";
+                table += "</div>";
+                table += "<center><button type='submit' class='ui-button ui-state-default ui-corner-all' id='btn-update' style='margin-top: 0.2em;' form='form_" + groups[group].event[i].Row + "'>Update & Approve</button></center>";
+                table += "</form>";
+                table += "</div>";
+                table += "</div>";
+                table += "</td></tr>";
+            }
+            table += "</tbody></table>"
+            html += table;
+        }
+        callback(html);
+    }
+    
+    function selectCatagory(tar, opt) {
+        $(tar + 'option').each(function (a, b) {
+            console.log(b);
+        });
+    }
+
+
+    var loadHtml = function (html) {
+        $("#tabs-pending div").html(html);
+        $(".row-details .rundates").multiDatesPicker({
+            dateFormat: 'mm/dd/yy',
+            minDate: $.datepicker.formatDate('mm/dd/yy', new Date()),
+            beforeShowDay: $.datepicker.noWeekends,
+        });
+        $(".row-display").children('td').hover(function () {
+            $(this).parent('tr').toggleClass('on-hover');
+        });
+        createDatePicker();
+
+        $("form").on('submit', function (event) {
+            $("button").attr("disabled", "disabled");
+            $('#container').show();
+            var formId = $(this).attr("id");
+            if (document.forms[formId].checkValidity()) {
+                console.log(formId);
+                event.preventDefault();
+                var data = {
+                    function: 'updateRequest',
+                    payload: {
+                        Row: $(this).data('row'),
+                        Status: 'Approved'
+                    }
+                };
+                $("#" + formId + " :input:not([readonly]), #" + formId + " textarea, #" + formId + ":selected").each(function () {
+                    if (data.payload[this.name] !== undefined) {
+                        if (!data.payload[this.name].push) {
+                            data.payload[this.name] = [data.payload[this.name]];
+                        }
+                        data.payload[this.name].push(this.value || '');
+                    } else {
+                        if ($(this).is("OPTION")) {
+                            data.payload["Category"] = this.value;
+                        } else {
+                            data.payload[this.name] = this.value || '';
+                        }
+                    }
+                });
+                setTimeout('$("button").removeAttr("disabled")', 2000);
+                postFormData(data);
+            } else {
+                event.preventDefault();
+                return false;
+            }
+        });
+
+        $(".event-table .tblgroup tbody tr.row-display:not(.row-details) td").hover(function () {
+            $(this).parent().children().toggleClass("ui-state-hover");
+        })
+
+        $(".row-display td:not(:first-child)").click(function (e) {
+            var row = $(this).parent('tr').data('row');
+            var _this = $('tr[data-row="' + row + '"]');
+
+            if ($(_this).next(".row-details").is(".expanded")) {
+                $(_this).next(".row-details").toggleClass("expanded collapsed");
+                $("td.ui-state-active").removeClass("ui-state-active");
+                return;
+            } else {
+                $(".expanded").toggleClass("expanded collapsed");
+                $("td.ui-state-active").removeClass("ui-state-active");
+            }
+            $(_this).next(".row-details").toggleClass("expanded collapsed");
+            $(_this).children("td").addClass("ui-state-active");
+        });
+    };
+
+    function createDatePicker() {
+        try {
+            $(".eventdates").datepicker({
+                dateFormat: 'mm/dd/yy',
+                beforeShowDay: $.datepicker.noWeekends,
+            });
+            $(".rundates").multiDatesPicker({
+                dateFormat: 'mm/dd/yy',
+                minDate: $.datepicker.formatDate('mm/dd/yy', new Date()),
+                beforeShowDay: $.datepicker.noWeekends,
+            });
+        } catch (ex) {
+            console.log(ex.message);
+        }
+    }
+
+    function convertDate(input) {
+        var y = input.substring(0, 4);
+        var mon = (parseInt(input.substring(4, 6),10) - 1);
+        var d = input.substring(6, 8);
+        var h = input.substring(8, 10);
+        var min = input.substring(10, 12);
+        var da = new Date(y, mon, d, h, min).toLocaleDateString('en-US');
+        return da;
+    }
+
+    Date.prototype.addMonth = function (month) {
+        var dat = new Date(this.valueOf());
+        dat.setDate(dat.getMonth() + month);
+        return dat;
+    };
+})();
+
---
+++ scripts/index.js
@@ -0,0 +1,104 @@
+﻿(function () {
+    var global = {};
+
+    window.googleapisService = function () {
+        var pHeader = { "alg": "RS256", "typ": "JWT" };
+        var sHeader = JSON.stringify(pHeader);
+
+        var pClaim = {};
+        pClaim.aud = "https://www.googleapis.com/oauth2/v3/token";
+        pClaim.iss = "1075942469255-rahtil667s666gbdkg3d24ndp749eojc@developer.gserviceaccount.com";
+        pClaim.sub = "mdavis@mrpkedu.org";
+        pClaim.scope = "https://www.googleapis.com/auth/script.send_mail https://www.googleapis.com/auth/script.storage https://www.googleapis.com/auth/spreadsheets";
+        pClaim.exp = KJUR.jws.IntDate.get("now + 1hour");
+        pClaim.iat = KJUR.jws.IntDate.get("now");
+
+        var sClaim = JSON.stringify(pClaim);
+
+        var key = "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDy/+mtpc7mW8rJ\ngCbUs1g/r2JtAUweXjIuDBvUwPfzyMC3pT4PRvTVZ3jkv2qAM2eRVd8FyF4F6IAp\nWOfiUytcTSHnx0bgU1e3k8JqdyvKufRGgWOypkrtCdH9hntHGOkt0JojyfpWoJ4s\n84MygrCD5xBb7D7Sw5wjugmCjhg3UWnM6JRHdxlcInWm8+0zLBMPlzNPGvkhKvkf\nm+KENIXAo4aCvd7s7OWLEUdEfm2kla4Cj8VOs775a40IuRlYCZWIvJHYJpxmR/tY\nTJGKdKQ/wsEhfZlRRaa5ehAUTQLOBaZHUjamPm8012eh/zQNBVy6ePwnWXlvCUyb\nXO9ME0eTAgMBAAECggEAEEu/NQj4jhQ7zjWM5Ri94090r1C19RdmEBZ5y1PBjRS6\nb9ZiCg49Hw6YWebIUp6f5Gw4z593Rrgn2B4BNvlS4BbAHlYL18FfEY2ULSWexzKf\nQhctQ27JeYn+Dh11qPfvs0XZAGtsIWEWGneeJcYD5mSZgUCfmezKTM8OEjZLQ3JN\nymQURnHNCcNBMfCKxfZOQTLOfbrfaTnPd08y3bclTk7muHjvMpaVz9F59n7Dng3U\n/PQ8YvggnwiiX2UNMJ9qwhyYuFJ1YMKgiK+hDOtYIxgINVkMt0CadFfE9hHxCgWT\nFpznssMpdH2mJhaO+deP+KsQFJXIldUMEvuEU3Ob0QKBgQD/2k4u1rgO7zuGdHe5\nQ94CZ9laFyOLT9JC/zNUH71WqHPquXV3OGmkO7EWCX57X9R7E7OnJp6QlLRX0Mq6\nxRI/AXwfLQ3aV6ymeo7l4wjPo2vnrN26XsvCExEo0ReDnUhkTpCL1TZpsxp8Syvf\ndlMcdlJ1S5tL0/SPc3ejPEAXWQKBgQDzI7a5Z04+lTFRzL2/pHOfVWAWWQe67rDM\n5QPVzzvFDoE2Ws5z9wtqaN8goaYQKQ+KQl/a3kCoeMvcvPHjdO7erm6XE/2wr7hw\nOXjWM4r9asKmHe+5pp5fnV5I1llfCiSzIvyYmrGvb7u3km4mX+lsOuMXGdfbCFFD\nlyxXevJkywKBgDaru1Ee6K2oSRvt/zdDXUk+T5+Emj/GirNuPNWZAqhlzn81qfgT\nwd8qMij3VmOekpTKHPV2tyNF41NwdiF6wu9brd4rjm49ldLtFWPiI5va28/hjG+q\nmfeag2wpYdDp5vIoJ5L5uGrVrq8okPNrIU9PqUcPJNwLKpMMpV/oG/nhAoGAfURI\nLwuHZ3zsNQ28MRFkNJIC+GCUN/4j/tx69lxXmfYYIk30khCUxBUV6DUAXtEY3Y3G\n3vek7meV+LssEJk9PGM4PQQirzZJWyBxHkLd+CRZ9sXzrX2nmPF2EybEUs/iCp0a\n8PC6XP2/BBwgPgsqMwW/3DYHXSTS4XTwdn20hrMCgYAtCYOjVYzOSW6MF3EpDi3s\nNNyyV/GZOZHu8+4X3s72zIdkkp729IgB2Iprh80fY18TTvxcak1vU7m71ZRrCYgc\n2Cd4l6/+U0BePyElB0hfP/2E1aF7+ZqIfuIk/kW5cpalFgqts0cwQ/dOPWuCXf3i\nJ6ciiLsr65DnFz0Es3NnXw\u003d\u003d\n-----END PRIVATE KEY-----\n";
+
+        var sJWS = KJUR.jws.JWS.sign(null, sHeader, sClaim, key);
+
+        var XHR = new XMLHttpRequest();
+        var urlEncodedData = "";
+        var urlEncodedDataPairs = [];
+
+        urlEncodedDataPairs.push(encodeURIComponent("grant_type") + '=' + encodeURIComponent("urn:ietf:params:oauth:grant-type:jwt-bearer"));
+        urlEncodedDataPairs.push(encodeURIComponent("assertion") + '=' + encodeURIComponent(sJWS));
+        urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
+
+        XHR.addEventListener('load', function (event) {
+            var response = JSON.parse(XHR.responseText);
+            global['oauth-token'] = response["access_token"];
+
+            gapi.auth.authorize({
+                client_id: '1075942469255-2tlk0uhhpemjl4g4mstj789pu7f2ql9u.apps.googleusercontent.com',
+                scope: "https://www.googleapis.com/auth/script.send_mail https://www.googleapis.com/auth/script.storage https://www.googleapis.com/auth/spreadsheets",
+                immediate: true
+            }, buildObjects);
+        });
+
+        XHR.addEventListener('error', function (event) {
+            console.log('Oops! Something went wrong.');
+        });
+
+        XHR.open('POST', 'https://www.googleapis.com/oauth2/v3/token');
+        XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        XHR.send(urlEncodedData);
+    };
+
+    window.buildObjects = function () {
+        var blocker = '<div id="blocker" class="ui-widget-overlay" style="display: none;"></div>';
+        var container = '<div id="container" class="ui-draggable ui-corner-all ui-widget-header" style="text-align: center; z-index: 1000; position: absolute; margin-left: -147px; left: 50%; top: 35%; width: 320px; display: none; font-size: 12px; border: 1px solid rgb(128, 128, 128);">';
+        container += '<div style="margin: 5%;"><center>';
+        container += '<img style="display: block; margin-top: 8px;" class="ui-corner-all ui-widget-content ui-widget" src="../images/gears-animation1.gif"></center>';
+        container += '<p>Please wait while we process your request.</p>';
+        container += '<div style="text-align: center">';
+        container += '<button id="close-container" class="ui-button ui-state-default ui-corner-all" style="display: none;">Close</button>';
+        container += '</div></div></div>';
+        var growl = '<div class="growlUI" style="display: none; cursor: default;"></div>';
+
+        $("body").append(blocker);
+        $("body").append(container);
+        $("body").append(growl);
+    }
+
+    window.postFormData = function (data) {
+        var scriptId = 'MX4ev5IFFXUd7DRHOn8f7c2uZymsaB7IG';
+        var payload = {
+            'function': data.function,
+            'parameters': data.payload,
+            'devMode': true
+        };
+
+        var req = gapi.client.request({
+            'root': 'https://script.googleapis.com/',
+            'path': 'v1/scripts/' + scriptId + ':run',
+            'method': 'POST',
+            'headers': { Authorization: 'Bearer ' + global['oauth-token'] },
+            'body': payload
+        });
+
+        req.execute(function (res) {
+            $("div#container").on("click", "#close-container", function () {
+                $("#container").remove();
+                $("#blocker").remove();
+                buildObjects;
+            });
+            if (res.error) {
+                $("div#container").prepend("<h5>Error</h5>");
+                $("div#container p").text("The following error occured while processing your request:  \n" + json.error);
+            } else {
+                var json = $.parseJSON(res.response.result);
+                if (!json.hasError) {
+                    $("div#container").prepend("<h5>Success!</h5>");
+                    $("div#container p").text("Your Bulletin Request was Successfully submitted.");
+                } else {
+                    $("div#container").prepend("<h5>Error</h5>");
+                    $("div#container p").text("The following error occured while processing your request:  \n" + json.message);
+                }
+            }
+            $("div#container button").show();
+        });
+    };
+})();
